78221c620790ecd060cccd57c83b3615f6516f92
Aayush Sinha <aayush.aryan@me.com>
2026-02-17 13:00:12 +0900

    Update sync auth for google

--- src/cli/sync.py
+++ src/cli/sync.py
@@ -36,8 +36,9 @@ def load_config():
     load_dotenv(env_file)
 
     return {
-        'google_credentials': os.getenv('GOOGLE_CREDENTIALS_PATH'),
-        'google_token': os.getenv('GOOGLE_TOKEN_PATH'),
+        'google_client_id': os.getenv('GOOGLE_CLIENT_ID'),
+        'google_client_secret': os.getenv('GOOGLE_CLIENT_SECRET'),
+        'google_refresh_token': os.getenv('GOOGLE_REFRESH_TOKEN'),
         'icloud_username': os.getenv('ICLOUD_USERNAME'),
         'icloud_password': os.getenv('ICLOUD_APP_PASSWORD'),
         'icloud_url': os.getenv('ICLOUD_CARDDAV_URL', 'https://contacts.icloud.com'),
@@ -72,8 +73,9 @@ def run_sync():
         logger.info("\n[1/5] Syncing Google Contacts...")
         try:
             google_client = GoogleContactsClient(
-                config['google_credentials'],
-                config['google_token']
+                config['google_client_id'],
+                config['google_client_secret'],
+                config['google_refresh_token']
             )
             google_sync = GoogleContactsSync(google_client, session)
             google_stats = google_sync.sync()
